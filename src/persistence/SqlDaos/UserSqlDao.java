dd3c7fce7cb51efe924f4b87a9325cdb253593af
sergi <sergi.castillo@students.salle.url.edu>
2025-04-11 10:49:16 +0200

    SlotsManager hecho queda repasarlo y Slot Dao en principio acabado

--- src/persistence/SqlDaos/UserSqlDao.java
+++ src/persistence/SqlDaos/UserSqlDao.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserSqlDao {
-    static Connection connection;
+     static Connection connection;
 
     public UserSqlDao () {
         try {
