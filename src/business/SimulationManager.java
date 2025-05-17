8a0ab256e2e8ed07794f750709a58df6262e1ff5
sergi <sergi.castillo@students.salle.url.edu>
2025-05-17 13:19:57 +0200

    SimulacioAcabada

---
+++ src/business/SimulationManager.java
@@ -0,0 +1,42 @@
+package business;
+
+import java.sql.SQLException;
+
+public class SimulationManager implements Runnable {
+    private boolean running;
+    private ParkingStatusManager parkingStatusManager;
+
+    public SimulationManager() {
+    }
+
+    @Override
+    public void run() {
+        running = true;
+        parkingStatusManager = new ParkingStatusManager();
+        long frequency = parkingStatusManager.calculateFrequency() * 1000L;
+        System.out.println("FREQ: "+frequency);
+        while (running) {
+            try {
+                Thread.sleep(frequency);
+                if (!parkingStatusManager.getFreeUnbookedSlots()) {
+                    parkingStatusManager.simulateEntry(); System.out.println("ENTRA VEHICLE\n");
+                } else {
+                    if (parkingStatusManager.calculateEntryOrExit()){
+                        parkingStatusManager.simulateEntry(); System.out.println("ENTRA VEHICLE\n");
+                    } else {
+                        parkingStatusManager.simulateExit();System.out.println("SALE UN VEHICULO");
+                    }
+                }
+            } catch (InterruptedException e) {
+                System.out.println("There was an interruption of the thread");
+                running = false;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public void stop() {
+        running = false;
+    }
+
+}
